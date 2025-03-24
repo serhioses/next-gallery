22f979d8c143bef8ce826db395331edc5f741833
serhioses <serega.mirosh@gmail.com>
2025-03-24 23:46:19 +0200

    Hide image gallery from unlogged users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import { SignedIn, SignedOut } from '@clerk/nextjs';
 import { db } from '~/server/db';
 
 export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
+    return (
+        <main className="">
+            <SignedOut>
+                <div className="h-full w-full text-center text-2xl">Please sign in above</div>
+            </SignedOut>
+            <SignedIn>
+                <Images />
+            </SignedIn>
+        </main>
+    );
+}
+
+async function Images() {
     const images = await db.query.images.findMany({
         orderBy(fields, operators) {
             return operators.desc(fields.id);
@@ -10,17 +24,15 @@ export default async function HomePage() {
     });
 
     return (
-        <main className="">
-            <div className="flex flex-wrap gap-4">
-                {images.map((image) => {
-                    return (
-                        <div key={image.id} className="flex w-48 flex-col">
-                            <img src={image.url} alt="" />
-                            <div>{image.name}</div>
-                        </div>
-                    );
-                })}
-            </div>
-        </main>
+        <div className="flex flex-wrap gap-4">
+            {images.map((image) => {
+                return (
+                    <div key={image.id} className="flex w-48 flex-col">
+                        <img src={image.url} alt="" />
+                        <div>{image.name}</div>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
