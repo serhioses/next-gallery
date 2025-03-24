145f9bb9e705b78a72dd365fdaf93f2e5ac740d6
sergii-myroshnyk-windmill <sergii.myroshnyk@windmill.digital>
2025-03-24 19:33:07 +0200

    Add todo list and update prettier settings

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,9 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
+  tabWidth: 4,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
 };
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,8 +1,8 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
-import { index, pgTableCreator } from "drizzle-orm/pg-core";
+import { sql } from 'drizzle-orm';
+import { index, pgTableCreator } from 'drizzle-orm/pg-core';
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -13,15 +13,15 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
 export const createTable = pgTableCreator((name) => `next-gallery_${name}`);
 
 export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [index("name_idx").on(t.name)],
+    'post',
+    (d) => ({
+        id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+        name: d.varchar({ length: 256 }),
+        createdAt: d
+            .timestamp({ withTimezone: true })
+            .default(sql`CURRENT_TIMESTAMP`)
+            .notNull(),
+        updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+    }),
+    (t) => [index('name_idx').on(t.name)],
 );
