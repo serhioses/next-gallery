521e31752f12e42474642f725eef1dfe373669a1
serhioses <serega.mirosh@gmail.com>
2025-03-27 14:42:39 +0200

    Set up Sentry

--- next.config.js
+++ next.config.js
@@ -2,19 +2,52 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import './src/env.js';
+import { withSentryConfig } from '@sentry/nextjs';
 
 /** @type {import("next").NextConfig} */
-const config = {
+const coreConfig = {
     images: {
-        remotePatterns: [{ hostname: '1rd1hl99eo.ufs.sh' }]
+        remotePatterns: [{ hostname: '1rd1hl99eo.ufs.sh' }],
     },
     typescript: {
         ignoreBuildErrors: true,
     },
     eslint: {
-        ignoreDuringBuilds: true
-    }
+        ignoreDuringBuilds: true,
+    },
 };
+
+const config = withSentryConfig(coreConfig, {
+    // For all available options, see:
+    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+    org: 'sergii-rb',
+    project: 'next-gallery',
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    // tunnelRoute: "/monitoring",
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+})
 
 export default config;
